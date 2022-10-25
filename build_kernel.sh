9ba45c15cb18f8c4042e4aa21bff63c70d2f8b9d
Victor Hugo <87090101+Vhmit@users.noreply.github.com>
2022-10-25 11:42:17 -0300

    build: directing parameters to source

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 make clean
 export ARCH=arm64
-export CROSS_COMPILE=/disco1/los16/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
-export CROSS_COMPILE_ARM32=/disco1/los16/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
+export CROSS_COMPILE=/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+export CROSS_COMPILE_ARM32=/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
 make jeter_defconfig
 make -j64
 
