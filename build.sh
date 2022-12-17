3d8d367bddfe8e4c965fe88a9386b75b0b1350a6
5H4ND <miqueiasantos20@gmail.com>
2022-12-16 21:17:35 -0300

    configs: regenerate jeter_defconfig
     * for 4.9

---
+++ build.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#
+# Copyright (C) 2020-2021 Adithya R.
+
+SECONDS=0 # builtin bash timer
+TC_DIR="$(pwd)/tc/clang"
+DEFCONFIG="jeter_defconfig"
+
+if test -z "$(git rev-parse --show-cdup 2>/dev/null)" &&
+   head=$(git rev-parse --verify HEAD 2>/dev/null); then
+	ZIPNAME="${ZIPNAME::-4}-$(echo $head | cut -c1-8).zip"
+fi
+
+export PATH="$TC_DIR/bin:$PATH"
+
+if ! [ -d "$TC_DIR" ]; then
+	echo "Azure Clang not found! Cloning to $TC_DIR..."
+	if ! git clone --depth=1 https://gitlab.com/Panchajanya1999/azure-clang "$TC_DIR"; then
+		echo "Cloning failed! Aborting..."
+		exit 1
+	fi
+fi
+
+if [[ $1 = "-r" || $1 = "--regen" ]]; then
+	make O=out ARCH=arm64 $DEFCONFIG savedefconfig
+	cp out/defconfig arch/arm64/configs/$DEFCONFIG
+	echo -e "\nSuccessfully regenerated defconfig at $DEFCONFIG"
+	exit
+fi
+
+if [[ $1 = "-c" || $1 = "--clean" ]]; then
+	rm -rf out
+fi
